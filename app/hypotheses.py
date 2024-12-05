b126b785e2d18090488a58501542b5da9be37228
Rudraksh Agarwal <rudrakshiitd@outlook.com>
2024-12-05 15:43:04 -0500

    add st to functions

--- app/hypotheses.py
+++ app/hypotheses.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 DB_FILE = "app/olympics_data.db"
 
-def hypothesis1():
+def hypothesis1(st):
     pass
 
-def hypothesis2():
+def hypothesis2(st):
     pass
 
-def hypothesis3():
+def hypothesis3(st):
     pass
 
-def hypothesis4():
+def hypothesis4(st):
     pass
