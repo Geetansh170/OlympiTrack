685d5661ea3d3d7531c6b58e7e72054233137d52
Rudraksh Agarwal <rudrakshiitd@outlook.com>
2024-12-05 15:24:36 -0500

    flow corrected

--- app/app.py
+++ app/app.py
@@ -5,17 +5,17 @@ from dbcrud import upload_data_in_db
 
 st.title("Olympic Trends Analysis")
 
-st.sidebar.header("Database Options")
-if st.sidebar.button("Upload Data from Raw CSV Files"):
+st.header("Upload from Raw CSV files")
+if st.button("Upload"):
     upload_data_in_db()
 
-st.sidebar.warning("This will overwrite all existing data.")
+st.warning("This will overwrite all existing data.")
 
-st.sidebar.header("Available Tables")
+st.header("Available Tables")
 tables = ["Athlete_Events_Details", "Event_Results", "Athlete_Biography",
           "Medal_Tally", "Games_Summary", "Population_Total", "Country_Profile"]
 
-selected_table = st.sidebar.selectbox("Select Table to View/Modify", tables)
+selected_table = st.selectbox("Select Table to View/Modify", tables)
 if selected_table:
     display_and_modify_table(st, selected_table)
 
